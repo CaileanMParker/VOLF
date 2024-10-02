c0f43ad131b7beb69b36be848ae8ca2d14a4dcc6
CaileanMParker <caileanparker@gmail.com>
2024-10-02 17:32:58 -0400

    small doc update

--- src/server/singleton_type.py
+++ src/server/singleton_type.py
@@ -15,7 +15,9 @@ class Singleton:
     __lock = Lock()
 
     def __new__(cls, *args, **kwargs) -> Self:  # pylint: disable=unused-argument
-        """Make this class (and its subclasses) singleton"""
+        """Create new instance only if one does not already exist, otherwise
+        return existing instance
+        """
         if cls.__instance is None:
             with cls.__lock:
                 if not cls.__instance:
